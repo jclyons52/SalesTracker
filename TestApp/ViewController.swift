97fe78f87473340815f497af7364339979b0a682
Joseph Lyons <jclyons52@gmail.com>
2016-04-20 08:20:07 +1000

    initial commit

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var salesTally: [Double] = []
+    var storeCostTally: [Double] = []
+    var productCareTally: [Bool] = []
+    @IBOutlet weak var storeCost: UITextField!
+    @IBOutlet weak var saleCost: UITextField!
+    @IBOutlet weak var commissionRate: UITextField!
+    @IBOutlet weak var productCare: UISwitch!
+    @IBOutlet weak var totalSales: UILabel!
+    @IBOutlet weak var profit: UILabel!
+    @IBOutlet weak var productCareRatio: UILabel!
+    @IBOutlet weak var totalCommission: UILabel!
+    @IBAction func save(sender: AnyObject) {
+        salesTally += saleCost.text!.isEmpty ? [0.0] : [Double(saleCost.text!)!]
+        storeCostTally += storeCost.text!.isEmpty ? [0.0] : [Double(storeCost.text!)!]
+        productCareTally += [productCare.on]
+        let totalProfit = salesTally.reduce(0, combine: +) - storeCostTally.reduce(0, combine: +)
+        profit.text = String(totalProfit)
+        totalSales.text = String(storeCostTally.reduce(0, combine: +))
+        print(productCareTally.filter({ $0 }).count, productCareTally.count, productCareTally)
+        productCareRatio.text = String(Double(productCareTally.filter({ $0 }).count) / Double(productCareTally.count))
+        let commission = commissionRate.text!.isEmpty ? 0.0 : Double(commissionRate.text!)!
+        totalCommission.text =  String(totalProfit * (commission / 100))
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
